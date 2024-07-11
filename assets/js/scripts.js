4294f3ccd400911ce0ce171e380bf2bda3b8b66b
Sohyun <164127801+parksohyunnn@users.noreply.github.com>
2024-07-11 14:58:32 +0900

    55

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,23 @@
-document.addEventListener('scroll', function () {
-    const svgContainer = document.querySelector('.castleLineContainer');
+document.addEventListener('DOMContentLoaded', function () {
     const svgPath = document.querySelector('.castleLinePath');
-    const rect = svgContainer.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
 
-    // 컨테이너가 뷰포트에 있는지 확인
-    if (rect.top <= windowHeight && rect.bottom >= 0) {
-        setTimeout(() => {
-            svgPath.style.strokeDashoffset = '0'; // 경로를 애니메이션으로 나타나게 함
-        }, 500); // 0.5초 지연
-    }
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                setTimeout(() => {
+                    svgPath.style.strokeDashoffset = '0'; // 경로를 애니메이션으로 나타나게 함
+                }, 500); // .5초 지연
+            } else {
+                svgPath.style.strokeDashoffset = '5000'; // 경로를 숨기기 위해 초기 상태로 되돌림
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    observer.observe(document.querySelector('.castleLineContainer'));
 });
