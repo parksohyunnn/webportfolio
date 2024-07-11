b8d2e64bca82ad18a4997a8d5e622629b9725644
Sohyun <164127801+parksohyunnn@users.noreply.github.com>
2024-07-11 15:52:08 +0900

    77

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+
+// 성 밖에 배경 흰색선이 내려오는 효과
 document.addEventListener('DOMContentLoaded', function () {
     const svgPath = document.querySelector('.castleLinePath');
     const observerOptions = {
@@ -20,4 +22,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const observer = new IntersectionObserver(observerCallback, observerOptions);
     observer.observe(document.querySelector('.castleLineContainer'));
+});
+
+// 성 이미지 등장 효과
+document.addEventListener('DOMContentLoaded', function () {
+    const castle1 = document.querySelector('.castle1');
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // 화면에 보이면 애니메이션 적용
+                castle1.style.right = '200px';
+                castle1.style.opacity = '1';
+                castle1.classList.add('bounce-animation');
+            } else {
+                // 화면에서 벗어나면 애니메이션 초기화 (필요 시)
+                castle1.style.right = '-1000px';
+                castle1.style.opacity = '0';
+                castle1.classList.remove('bounce-animation');
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    observer.observe(document.querySelector('.castle-img'));
 });
