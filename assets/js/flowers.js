1551d35f77aba27c1617765ed98a256cef8c4301
Sohyun <164127801+parksohyunnn@users.noreply.github.com>
2024-08-05 10:16:33 +0900

    ㅇㄴㅇㄴ

---
+++ assets/js/flowers.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // skill
+    // 메인 꽃 배경
+    gsap.set(".mainFlower .flowerImg", { opacity: 0, y: 45 });
+    // 메인 왼쪽 꽃
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower2", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower3", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower4", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower5", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower6", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower7", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower8", { opacity: 0, y: 5 });
+    gsap.set(".mainFlower .flower9", { opacity: 0 });
+    gsap.set(".mainFlower .flower10", { opacity: 0 });
+    gsap.set(".mainFlower .flower11", { opacity: 0 });
+    gsap.set(".mainFlower .flower12", { opacity: 0 });
+    gsap.set(".mainFlower .flower13", { opacity: 0 });
+    gsap.set(".mainFlower .flower14", { opacity: 0 });
+    gsap.set(".mainFlower .flower15", { opacity: 0 });
+    gsap.set(".mainFlower .flower16", { opacity: 0 });
+    gsap.set(".mainFlower .flower17", { opacity: 0 });
+    gsap.set(".mainFlower .flower18", { opacity: 0 });
+    gsap.set(".mainFlower .flower19", { opacity: 0 });
+    gsap.set(".mainFlower .flower20", { opacity: 0 });
+    gsap.set(".mainFlower .flower21", { opacity: 0 });
+    gsap.set(".mainFlower .flower22", { opacity: 0 });
+    // 메인 오른쪽 꽃
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0, y: -15 });
+    gsap.set(".mainFlower .flower1", { opacity: 0 });
+    const tl = gsap.timeline();
+
+    // 홈
+    // 메인 꽃 배경
+    setTimeout(() => {
+        tl.to(".mainFlower .flowerImg", { opacity: 1, y: 0, stagger: 1, duration: 2, delay: 2, ease: "power4.out" });
+        // 메인 왼쪽 꽃
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.3 });
+        // 메인 오른쪽 꽃
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, y: 0, stagger: 0.5, duration: 0.2, ease: "power2.out" });
+        tl.to(".mainFlower .flower1", { opacity: 1, stagger: 0.5, duration: 0.1, ease: "power2.out" });
+    }, 1000);
+});
